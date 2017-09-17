10ceb421c5c6c1d1d5a350ac77c28ba2ab618103
Richard Nichol <richardonichol@gmail.com>
2017-09-17 16:35:50 +1000

    Failing test for deuce

--- spec/match_spec.rb
+++ spec/match_spec.rb
@@ -13,4 +13,28 @@ describe Match do
     @match.pointWonBy("player 1");
     expect(@match.score).to eq('0-0, 15-0')
   end
+
+  it "shows correct score after two points" do
+    @match.pointWonBy("player 1");
+    @match.pointWonBy("player 2");
+    expect(@match.score).to eq('0-0, 15-15')
+  end
+
+  it "shows correct score after four points" do
+    @match.pointWonBy("player 1");
+    @match.pointWonBy("player 2");
+    @match.pointWonBy("player 1");
+    @match.pointWonBy("player 1");
+    expect(@match.score).to eq('0-0, 40-15')
+  end
+
+  it "shows correct score at deuce" do
+    @match.pointWonBy("player 1");
+    @match.pointWonBy("player 2");
+    @match.pointWonBy("player 1");
+    @match.pointWonBy("player 1");
+    @match.pointWonBy("player 2");
+    @match.pointWonBy("player 2");
+    expect(@match.score).to eq('0-0, Deuce')
+  end
 end
